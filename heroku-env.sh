8652072b1e71efeb70d5af5c492dd9032794dca3
Tim Miller <broadcaststorm@users.noreply.github.com>
2022-01-13 17:21:23 -0500

    Debug heroku-env.sh script (make safe for zsh too). Add heroku-local.sh for envs needed for local heroku runs

--- heroku-env.sh
+++ heroku-env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-if -z "${SECRETS_DIR}"; then
+if test -z "${SECRETS_DIR}"; then
     echo "Set a SECRETS_DIR location for token.txt"
     exit 1
 fi
@@ -8,7 +8,7 @@ fi
 export WEBEX_TEAMS_ACCESS_TOKEN=$(cat ${SECRETS_DIR}/token.txt)
 export WEBEX_TEAMS_ROOM_TITLE='GVE RTP Lab - Scenario Management'
 
-if -z "${WEBEX_TEAMS_ACCESS_TOKEN}"; then
+if test -z "${WEBEX_TEAMS_ACCESS_TOKEN}"; then
     echo "Application requires a Webex Bot Token to be defined."
     exit 1
 fi
@@ -16,5 +16,5 @@ fi
 heroku config:set WEBEX_TEAMS_ACCESS_TOKEN=${WEBEX_TEAMS_ACCESS_TOKEN}
 # Setting WEBEX_TEAMS_ACCESS_TOKEN and restarting ... done, v5
 
-heroku config:set WEBEX_TEAMS_ROOM_TITLE="${WEBEX_TEAMS_ROOT_TITLE}
+heroku config:set WEBEX_TEAMS_ROOM_TITLE="${WEBEX_TEAMS_ROOT_TITLE}"
 # Setting WEBEX_TEAMS_ROOM_TITLE and restarting ... done, v6
---
+++ heroku-local.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+export HEROKU_APP_NAME="local_webex_bot"
+
